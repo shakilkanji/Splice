ce9fad9282e30aeb99b09ce4c004fd3551bbf24d
Shakil Kanji <shakil.kanji@gmail.com>
2016-11-01 02:56:09 -0400

    Initial Commit

---
+++ Splice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Splice
+//
+//  Created by Shakil Kanji on 2016-11-01.
+//  Copyright © 2016 Shakil Kanji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
